c4fe4bbee50cd7344312f98b9474d796b128573a
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2018-04-18 17:07:34 +0100

    Main page content.

--- TDoR/views/pages/home.php
+++ TDoR/views/pages/home.php
@@ -1,3 +1,20 @@
-<p>Hello!<p>
 
-<p>This is the home page. If this bit works, it's a good start.</p>
+<div align="center"><img src="/images/tdor_candle_jars.jpg" alt="Every year on or around 20th November people worldwide gather for the Transgender day of Remembrance" style="" /></div>
+<p>&nbsp;</p>
+
+<p>Every year on or around 20th November people worldwide gather for the <a href="https://tdor.info/about-2/" target="_blank">Transgender day of Remembrance</a> to remember the trans people we have lost to murder and violence in the past year.</p>
+
+<p>Reports are collated by the <a href="https://transrespect.org/en/research/trans-murder-monitoring/" target="_blank">Trans Murder Monitoring Project</a> and published by <a href="https://tdor.tgeu.org/" target="_blank">Transgender Europe</a> (TGEU) in early November of each year..</p>
+
+<p>This site gives details of the trans people known to have been killed worldwide, as collated from TGEU reports and those identified by trans activists worldwide. It's very much a work in progress, so only includes a small subset of sample data at present.</p>
+
+<p>As well as those identified by TGEU, details are also given for other killings, deaths in custody and those known lost to suicide where known.</p>
+
+<p>A big thanks are due to the members and admins of the <a href="https://www.facebook.com/groups/1570448163283501/" target="_blank">Trans Violence News</a> Facebook group, without whose support this site would not be possible.</p>
+
+<p><i>Please</i> don't read anything into the raw numbers. What matters is the lives lost, and their stories. Be warned though - some of their stories are horrific.</p>
+
+<p>&nbsp;</p>
+<p><b>[TRIGGER WARNING: VIOLENCE. MURDER]</b></p>
+
+<a href="/?controller=posts&action=index"><b>Reports</b></a>
