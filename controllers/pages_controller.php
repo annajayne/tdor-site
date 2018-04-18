a824b7d77e76514884f05ffa3a7bdf41e39f73ec
Anna-Jayne Metcalfe <anna@riverblade.co.uk>
2018-04-18 10:45:32 +0100

    Removed firstname/lastname nonsense on the homepage.

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -9,9 +9,6 @@
     {
         public function home()
         {
-            $first_name = 'Anna';
-            $last_name  = 'Moo';
-
             require_once('views/pages/home.php');
         }
 
