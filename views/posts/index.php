5ac3a3124e9fabdd317cb287b7f8ec147aad8b1f
Anna-Jayne Metcalfe <anna@riverblade.co.uk>
2018-04-18 09:36:21 +0100

    Whitespace changes only.

--- views/posts/index.php
+++ views/posts/index.php
@@ -1,13 +1,13 @@
 <p>Here is a list of all posts:</p>
 
-    <?php foreach($posts as $post)
+    <?php foreach ($posts as $post)
     {
     ?>
 
-  <p>
-    <?php echo $post->author; ?>
-    <a href='?controller=posts&action=show&id=<?php echo $post->id; ?>'>See content</a>
-  </p>
+      <p>
+        <?php echo $post->author; ?>
+        <a href='?controller=posts&action=show&id=<?php echo $post->id; ?>'>See content</a>
+      </p>
 
     <?php
     }
