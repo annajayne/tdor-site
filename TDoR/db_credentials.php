458dc26de7b9f5bdf3b1f50e240fd40e055b2b5b
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2018-04-18 17:19:27 +0100

    Corrected database name.

--- TDoR/db_credentials.php
+++ TDoR/db_credentials.php
@@ -2,10 +2,10 @@
 
     class db_credentials
     {
-        public $servername = "localhost";
-        public $dbname = "php_mvc";
-        public $username = "tester";
-        public $password = "test";
+        public $servername  = "localhost";
+        public $dbname      = "tdor";
+        public $username    = "tester";
+        public $password    = "test";
 
         public $pdo_options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
     }
