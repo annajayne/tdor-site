c7d17a7faa5417958a227cf4fea8ca5bbcf54a21
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2018-05-23 21:25:26 +0100

    Added a homepage link to deploy.php

--- Deploy/deploy.php
+++ Deploy/deploy.php
@@ -9,6 +9,8 @@
             $zip->close();
 
             echo "Extracted $pathname<br><br>";
+
+            echo '<a href="index.php?controller=pages&action=home"><b>Homepage</b></a><br><br>';
             
             echo '<a href="index.php?controller=pages&action=rebuild"><b>Rebuild Database</b></a>';
         }
