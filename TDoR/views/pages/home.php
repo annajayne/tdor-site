dcdeaa3b530bf90912ea0f0f341e5057e08e1a8b
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2018-05-25 18:54:53 +0100

    Updated homepage text.

--- TDoR/views/pages/home.php
+++ TDoR/views/pages/home.php
@@ -1,17 +1,15 @@
 
-<p>Every year on or around 20th November people worldwide gather for the <a href="https://tdor.info/about-2/" target="_blank">Transgender day of Remembrance</a> to remember trans people lost to murder and violence in the past year.</p>
-
-<p>Reports are collated by the <a href="https://transrespect.org/en/research/trans-murder-monitoring/" target="_blank">Trans Murder Monitoring Project</a> and published by <a href="https://tdor.tgeu.org/" target="_blank">Transgender Europe</a> (TGEU) in early November of each year. All material presented is gathered from publicly available sources.</p>
-
-<p>This site gives details of the trans people known to have been killed worldwide, as collated from TGEU reports and those identified by trans activists directly. The intention is that this site can act as a supporting resource for anyone organising a Transgender Day of Remembrance Event.</p>
+<p>TDoR reports are collated by the <a href="https://transrespect.org/en/research/trans-murder-monitoring/" target="_blank">Trans Murder Monitoring Project</a> and published by <a href="https://tdor.tgeu.org/" target="_blank">Transgender Europe</a> (TGEU) in early November of each year. In addition, trans activists and local organisations worldwide collate data on a continuous basis so that we don't have to wait until November to learn who we've lost.</p>
+    
+<p>The intention is that the information presented on this site can act as a supporting resource for anyone organising a Transgender Day of Remembrance (TDoR) Event.</p>
 
 <p>A big thanks are due to the members and admins of the <a href="https://www.facebook.com/groups/1570448163283501/" target="_blank">Trans Violence News</a> Facebook group, without whose support this site would not be possible.</p>
 
-<p><i>Please</i> don't read anything into the raw numbers. What matters is the lives lost, and their stories. Be warned though - some of their stories are horrific.</p>
+<p><p>All material presented is gathered from publicly available sources. <i>Please</i> don't read anything into the raw numbers. What matters is the lives lost, and their stories. Be warned though - some of their stories are horrific.</p>
 
-<p>&nbsp;</p>
 <p><b>[TRIGGER WARNING: VIOLENCE. MURDER]</b></p>
 
-<p><i><b>Important note:</b> This site is in the early stages of development, and as such not everything works yet. In particular, links to reports on specific victims are likely to change. If you need to refer to reports relating to a particular victim, please take a note of their name and the corresponding date to ensure that you can find them again.</i></p>
+<p>&nbsp;</p>
+<p><i><b>Important note:</b> This site is in the early stages of development, and as such not everything works yet. In particular, links to reports on specific victims are likely to change. If you need to refer to reports relating to a particular victim, please make a note of their name and the corresponding date to ensure that you can find them again.</i></p>
 
 <a href="/?category=reports&action=index"><b>Reports</b></a>
