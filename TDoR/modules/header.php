f16c9504c99265d9983c4f2430822233e8741661
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2018-06-01 13:01:23 +0100

    Fixed a warning.

--- TDoR/modules/header.php
+++ TDoR/modules/header.php
@@ -41,7 +41,7 @@
     function get_metadata($controller, $action)
     {
         $host     = (isset($_SERVER['HTTPS']) ? 'https' : 'http')."://$_SERVER[HTTP_HOST]";
-        $uri      = $_SERVER[REQUEST_URI];
+        $uri      = $_SERVER['REQUEST_URI'];
 
         $metadata = new page_metadata();
 
