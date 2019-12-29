9701aa2a166c8ed73bbe5ee4bd668c9abde57487
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2019-12-29 13:58:42 +0000

    Replaced calls to header() with redirect_to() in the "Account" pages, as headers have already been sent by the time these pages are now loaded.

--- Projects/TDoR/src/views/account/change_password.php
+++ Projects/TDoR/src/views/account/change_password.php
@@ -5,176 +5,179 @@
     // Check if the user is logged in, otherwise redirect to login page
     if (!is_logged_in() )
     {
-        header("location: /account/login");
-        exit;
-    }
-
-
-    $form_action_url = '/account/change_password';
-
-    $password_changed = false;
-    
-    $username = get_logged_in_username();
-
-    $db             = new db_credentials();
-
-    $users_table    = new Users($db);
-    $user           = $users_table->get_user($username);
-
-    if ($user->username === $username)
-    {
-        // Define variables and initialize with empty values
-        $old_password = $new_password = $confirm_password = '';
-        $old_password_err = $new_password_err = $confirm_password_err = $password_change_err = '';
-
-        // Processing form data when form is submitted
-        if ($_SERVER["REQUEST_METHOD"] == 'POST')
+        if (redirect_to('/account/login') )
         {
-            // Validate old password
-            if (empty(trim($_POST['old_password']) ) )
-            {
-                $old_password_err = 'Please enter your existing password.';
-            }
-            else
-            {
-                $old_password = trim($_POST['old_password']);
-
-                $temp = password_hash($old_password, PASSWORD_DEFAULT);
-
-                if (!password_verify($old_password, $user->hashed_password) )
-                {
-                    $old_password_err = 'Incorrect password entered.';
-                }
-            }
-
-            // Validate new password
-            if (empty(trim($_POST['new_password']) ) )
-            {
-                $new_password_err = 'Please enter the new password.';
-            }
-            elseif (strlen(trim($_POST['new_password']) ) < 6)
-            {
-                $new_password_err = 'The new password must have at least 6 characters.';
-            }
-            else
-            {
-                $new_password = trim($_POST['new_password']);
-            }
-
-            // Validate confirm password
-            if (empty(trim($_POST['confirm_password']) ) )
-            {
-                $confirm_password_err = 'Please confirm the password.';
-            }
-            else
-            {
-                $confirm_password = trim($_POST['confirm_password']);
-                
-                if (empty($new_password_err) && ($new_password != $confirm_password) )
-                {
-                    $confirm_password_err = 'The passwords did not match.';
-                }
-            }
-
-            // Check input errors before updating the database
-            if (empty($old_password_err) && empty($new_password_err) && empty($confirm_password_err) )
-            {
-                $user->hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
-                
-                if ($users_table->update_user($user) )
-                {
-                    // Password updated successfully.
-                    $password_changed = true;
-                }
-                else
-                {
-                    $password_change_err = 'Oops! Something went wrong. Please try again.';
-                }
-            }
+            exit;
         }
-    }
-
-
-    echo '<h2>Change Password</h2>';
-
-    if ($password_changed)
-    {
-        echo '<p>&nbsp;</p>';
-        echo '<p>Your password has been changed.</p>';
-        echo '<p><a href="/account"><b>Click here to continue</b></a>.</p>';
     }
     else
     {
-        echo '<p>Please fill out this form to change your password.</p>';
-        echo "<form action='$form_action_url' method='post'>";
+        $form_action_url = '/account/change_password';
 
+        $password_changed = false;
+    
+        $username = get_logged_in_username();
 
-        // Old password
-        echo   '<div class="clearfix">';
-        echo     '<div class="grid_2">';
-        echo       '<label>Old Password</label>';
-        echo     '</div>';
+        $db             = new db_credentials();
 
-        echo     '<div class="grid_10">';
-        echo       "<input type='password' name='old_password' value='$old_password' />";
+        $users_table    = new Users($db);
+        $user           = $users_table->get_user($username);
 
-        if (!empty($old_password_err) )
+        if ($user->username === $username)
         {
-            echo   "<p class='account-error'>$old_password_err</p>";
+            // Define variables and initialize with empty values
+            $old_password = $new_password = $confirm_password = '';
+            $old_password_err = $new_password_err = $confirm_password_err = $password_change_err = '';
+
+            // Processing form data when form is submitted
+            if ($_SERVER["REQUEST_METHOD"] == 'POST')
+            {
+                // Validate old password
+                if (empty(trim($_POST['old_password']) ) )
+                {
+                    $old_password_err = 'Please enter your existing password.';
+                }
+                else
+                {
+                    $old_password = trim($_POST['old_password']);
+
+                    $temp = password_hash($old_password, PASSWORD_DEFAULT);
+
+                    if (!password_verify($old_password, $user->hashed_password) )
+                    {
+                        $old_password_err = 'Incorrect password entered.';
+                    }
+                }
+
+                // Validate new password
+                if (empty(trim($_POST['new_password']) ) )
+                {
+                    $new_password_err = 'Please enter the new password.';
+                }
+                elseif (strlen(trim($_POST['new_password']) ) < 6)
+                {
+                    $new_password_err = 'The new password must have at least 6 characters.';
+                }
+                else
+                {
+                    $new_password = trim($_POST['new_password']);
+                }
+
+                // Validate confirm password
+                if (empty(trim($_POST['confirm_password']) ) )
+                {
+                    $confirm_password_err = 'Please confirm the password.';
+                }
+                else
+                {
+                    $confirm_password = trim($_POST['confirm_password']);
+                
+                    if (empty($new_password_err) && ($new_password != $confirm_password) )
+                    {
+                        $confirm_password_err = 'The passwords did not match.';
+                    }
+                }
+
+                // Check input errors before updating the database
+                if (empty($old_password_err) && empty($new_password_err) && empty($confirm_password_err) )
+                {
+                    $user->hashed_password = password_hash($new_password, PASSWORD_DEFAULT);
+                
+                    if ($users_table->update_user($user) )
+                    {
+                        // Password updated successfully.
+                        $password_changed = true;
+                    }
+                    else
+                    {
+                        $password_change_err = 'Oops! Something went wrong. Please try again.';
+                    }
+                }
+            }
         }
-        echo     '</div>';
-        echo   '</div>';
 
 
-        // New password
-        echo   '<div class="clearfix">';
-        echo     '<div class="grid_2">';
-        echo       '<label>New Password</label>';
-        echo     '</div>';
+        echo '<h2>Change Password</h2>';
 
-        echo     '<div class="grid_10">';
-        echo       "<input type='password' name='new_password' value='$new_password' />";
-
-        if (!empty($new_password_err) )
+        if ($password_changed)
         {
-            echo   "<p class='account-error'>$new_password_err</p>";
+            echo '<p>&nbsp;</p>';
+            echo '<p>Your password has been changed.</p>';
+            echo '<p><a href="/account"><b>Click here to continue</b></a>.</p>';
         }
-        echo     '</div>';
-        echo   '</div>';
-
-
-        // Confirm new password
-        echo   '<div class="clearfix">';
-        echo     '<div class="grid_2">';
-        echo       '<label>Confirm Password</label>';
-        echo     '</div>';
-
-        echo     '<div class="grid_10">';
-        echo       '<input type="password" name="confirm_password" />';
-
-        if (!empty($confirm_password_err) )
+        else
         {
-            echo   "<p class='account-error'>$confirm_password_err</p>";
+            echo '<p>Please fill out this form to change your password.</p>';
+            echo "<form action='$form_action_url' method='post'>";
+
+
+            // Old password
+            echo   '<div class="clearfix">';
+            echo     '<div class="grid_2">';
+            echo       '<label>Old Password</label>';
+            echo     '</div>';
+
+            echo     '<div class="grid_10">';
+            echo       "<input type='password' name='old_password' value='$old_password' />";
+
+            if (!empty($old_password_err) )
+            {
+                echo   "<p class='account-error'>$old_password_err</p>";
+            }
+            echo     '</div>';
+            echo   '</div>';
+
+
+            // New password
+            echo   '<div class="clearfix">';
+            echo     '<div class="grid_2">';
+            echo       '<label>New Password</label>';
+            echo     '</div>';
+
+            echo     '<div class="grid_10">';
+            echo       "<input type='password' name='new_password' value='$new_password' />";
+
+            if (!empty($new_password_err) )
+            {
+                echo   "<p class='account-error'>$new_password_err</p>";
+            }
+            echo     '</div>';
+            echo   '</div>';
+
+
+            // Confirm new password
+            echo   '<div class="clearfix">';
+            echo     '<div class="grid_2">';
+            echo       '<label>Confirm Password</label>';
+            echo     '</div>';
+
+            echo     '<div class="grid_10">';
+            echo       '<input type="password" name="confirm_password" />';
+
+            if (!empty($confirm_password_err) )
+            {
+                echo   "<p class='account-error'>$confirm_password_err</p>";
+            }
+
+            echo     '</div>';
+            echo   '</div>';
+
+
+
+            echo   '<div class="clearfix">';
+            echo     '<div class="grid_2" ></div>';
+            echo     '<div class="grid_10">';
+            echo       '<input type="submit" class="button-blue" value="Submit" />';
+            echo       '<a class="button-gray" href="/account">Cancel</a>';
+
+            if (!empty($password_change_err) )
+            {
+                echo   "<p class='account-error'>$password_change_err</p>";
+            }
+            echo     '</div>';
+            echo   '</div>';
+            echo '</form>';
         }
-
-        echo     '</div>';
-        echo   '</div>';
-
-
-
-        echo   '<div class="clearfix">';
-        echo     '<div class="grid_2" ></div>';
-        echo     '<div class="grid_10">';
-        echo       '<input type="submit" class="button-blue" value="Submit" />';
-        echo       '<a class="button-gray" href="/account">Cancel</a>';
-
-        if (!empty($password_change_err) )
-        {
-            echo   "<p class='account-error'>$password_change_err</p>";
-        }
-        echo     '</div>';
-        echo   '</div>';
-        echo '</form>';
     }
 
 ?>
--- Projects/TDoR/src/views/account/welcome.php
+++ Projects/TDoR/src/views/account/welcome.php
@@ -5,72 +5,75 @@
      */
 
 
-
     if (!is_logged_in() )
     {
-        header("location: /account/login");
-        exit;
+        if (redirect_to('/account/login') )
+        {
+            exit;
+        }
     }
-
-    $username       = $_SESSION['username'];
-    $api_key        = '';
-
-    $is_api_user    = is_api_user();
-    $is_editor      = is_editor_user();
-    $is_admin       = is_admin_user();
-
-    if ($is_api_user && isset($_SESSION['api_key']) )
+    else
     {
-        $api_key = $_SESSION['api_key'];
+        $username       = $_SESSION['username'];
+        $api_key        = '';
+
+        $is_api_user    = is_api_user();
+        $is_editor      = is_editor_user();
+        $is_admin       = is_admin_user();
+
+        if ($is_api_user && isset($_SESSION['api_key']) )
+        {
+            $api_key = $_SESSION['api_key'];
+        }
+
+        $roles = '';
+        if ($is_api_user)
+        {
+            $roles = 'API user; ';
+        }
+
+        if ($is_editor)
+        {
+            $roles .= 'Editor; ';
+        }
+
+        if ($is_admin)
+        {
+            $roles .= 'Admin; ';
+        }
+
+        $roles = rtrim($roles, '; ');
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////
+        // Form content
+
+        echo '<h1>Welcome, <b>'.htmlspecialchars($username).'</b> ('.$roles.') </h1>';
+        echo '<p>&nbsp;</p>';
+
+        if ($is_api_user)
+        {
+            echo "<h3>API key: $api_key</h3>";
+        }
+        echo '<p>&nbsp;</p>';
+
+        echo '<p>';
+        echo   '<a href="/" class="button-blue">Homepage</a>&nbsp;';
+        echo   '<a href="/reports" class="button-dkred">Reports</a>&nbsp;';
+
+        if (!$is_editor)
+        {
+            $subject = CONTACT_SUBJECT_HELPING_OUT;
+
+            $url = '/pages/contact?subject='.urlencode($subject);
+
+            echo "<a href='$url' class='button-green' rel='nofollow'>Apply to become an editor</a>";
+        }
+
+        echo   '<a href="/account/change_password" class="button-blue">Change Password</a>&nbsp;';
+        echo   '<a href="/account/logout" class="button-orange">Logout</a>';
+        echo '</p>';
     }
-
-    $roles = '';
-    if ($is_api_user)
-    {
-        $roles = 'API user; ';
-    }
-
-    if ($is_editor)
-    {
-        $roles .= 'Editor; ';
-    }
-
-    if ($is_admin)
-    {
-        $roles .= 'Admin; ';
-    }
-
-    $roles = rtrim($roles, '; ');
-
-
-
-    ////////////////////////////////////////////////////////////////////////////////
-    // Form content
-
-    echo '<h1>Welcome, <b>'.htmlspecialchars($username).'</b> ('.$roles.') </h1>';
-    echo '<p>&nbsp;</p>';
-
-    if ($is_api_user)
-    {
-        echo "<h3>API key: $api_key</h3>";
-    }
-    echo '<p>&nbsp;</p>';
-
-    echo '<p>';
-    echo   '<a href="/" class="button-blue">Homepage</a>&nbsp;';
-    echo   '<a href="/reports" class="button-dkred">Reports</a>&nbsp;';
-
-    if (!$is_editor)
-    {
-        $subject = CONTACT_SUBJECT_HELPING_OUT;
-
-        $url = '/pages/contact?subject='.urlencode($subject);
-
-        echo "<a href='$url' class='button-green' rel='nofollow'>Apply to become an editor</a>";
-    }
-
-    echo   '<a href="/account/change_password" class="button-blue">Change Password</a>&nbsp;';
-    echo   '<a href="/account/logout" class="button-orange">Logout</a>';
-    echo '</p>';
 
 ?>
