c4f85007b7a25c87e2dfe9f8c54379b43de5497b
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2020-04-23 13:50:16 +0100

    The error page now dispays the HTTP status code if available

--- Projects/TDoR/src/views/pages/error.php
+++ Projects/TDoR/src/views/pages/error.php
@@ -1,12 +1,40 @@
 <?php
     /**
-     * The error page.
+     * Error page
+     *
      */
+
+    require_once('misc.php');
+
+
+    $details = '';
+    $url     = get_url();
+
+    $status_code = http_response_code();
+
+    if ( ($status_code == 200) && isset($_GET['code']) && isset($_GET['url']) )
+    {
+        $status_code    = $_GET['code'];
+
+        $url            = urldecode($_GET['url']);
+    }
+
+    if ( ($status_code > 0) && ($status_code >= 400) )
+    {
+        $status = HTTPStatus($status_code);
+
+        $details = $status['error'];
+    }
 
     echo '<h2>Error</h2>';
     echo '<div align="center">';
     echo   '<img src="/images/error.jpg" alt="It looks like something went wrong. Sorry!" />';
     echo '</div>';
+
+    if (!empty($details) )
+    {
+        echo "<br>Details: <b>$details</b><br>URL: <span style='font-size: 0.8em;'>$url</span>";
+    }
 
     echo '<p>&nbsp;</p>';
     echo "<p>This is the error page, so it looks like something went wrong. Our apologies.</p>";
