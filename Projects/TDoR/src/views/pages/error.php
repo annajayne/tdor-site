ab2638577cd09bb4a421c3570634897b8d063217
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2020-04-23 13:45:09 +0100

    Updated the "error" page

--- Projects/TDoR/src/views/pages/error.php
+++ Projects/TDoR/src/views/pages/error.php
@@ -2,8 +2,13 @@
     /**
      * The error page.
      */
+
+    echo '<h2>Error</h2>';
+    echo '<div align="center">';
+    echo   '<img src="/images/error.jpg" alt="It looks like something went wrong. Sorry!" />';
+    echo '</div>';
+
+    echo '<p>&nbsp;</p>';
+    echo "<p>This is the error page, so it looks like something went wrong. Our apologies.</p>";
+
 ?>
-
-<p>Oops, this is the error page.</p>
-
-<p>It looks like something went wrong. Our bad :(</p>
