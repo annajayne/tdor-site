73a2e3b08f15f6c9c92415fea32030c54389d086
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2020-11-24 21:39:21 +0000

    The Blog page now displays tiles with a summary of each blogpost rather than its full contents
    --HG--
    branch : blogpost support

--- Projects/TDoR/src/views/blog/index.php
+++ Projects/TDoR/src/views/blog/index.php
@@ -7,6 +7,80 @@
     require_once('util/blog_utils.php');
 
 
+
+    function get_first_n_words($longtext, $wordcount)
+    {
+        // remove redundant Windows CR
+        $longtext = preg_replace ("/\r/", "", $longtext);
+
+        // A space to an end, just in case
+        $longtext = $longtext . " ";
+
+        //  Regular expression for a word
+        $wordpattern = "([\w\(\)\.,;?!-_«»\"\'’]*[ \n]*)";
+
+        // Determine how many words are in the text
+        $maxwords = preg_match_all ("/" . $wordpattern . "/", $longtext, $words);
+
+        //  Make sure that the maximum number of available words is matched
+        $wordcount = min($wordcount, $maxwords);
+
+        // Create a regular expression for the desired number of words
+        $pattern = "/" . $wordpattern . "{0," . $wordcount . "}/";
+
+        // Read the desired number of words
+        $match = preg_match ($pattern, $longtext, $shorttext);
+
+        // Get the right result out of the result array
+        $shorttext = $shorttext[0];
+
+        return $shorttext;
+    }
+
+
+    /**
+     * Get a subtitle for the given blogpost (as this isn't a DB property yet, we create one from the content)
+     *
+     * @param Blogpost   $blogpost      The blogposts for which a subtitle should be returned.
+     * @return string                   The subtitle.
+     */
+    function get_blogpost_subtitle($blogpost)
+    {
+        $parsedown                      = new ParsedownExtraPlugin();
+
+        $parsedown->links_attr          = array();
+        $parsedown->links_external_attr = array('rel' => 'nofollow', 'target' => '_blank');
+
+        $content                        = $parsedown->text($blogpost->content); 
+
+        $subtitle                       = str_replace("<br />", " ", $content);
+        $subtitle                       = strip_tags($subtitle, "");
+        $subtitle                       = get_first_n_words($subtitle, 40).'...';
+
+        return $subtitle;
+    }
+
+
+    /**
+     * Show a summary tile for the given blockpost
+     *
+     * @param Blogpost   $blogpost      The blogposts for which a summary tile should be shown.
+     */
+    function show_blogpost_summary($blogpost)
+    {
+        $title      = !empty($blogpost->title) ? $blogpost->title : '(untitled)';
+        $subtitle   = get_blogpost_subtitle($blogpost);
+        $thumbnail  = '/images/tdor_candle_jars.jpg';
+
+        // TODO: add a thumbnail image here
+        echo '<div class="grid_6">';
+        echo   "<p><a href='$blogpost->permalink' title='$title'><b>$title</b></a></p>";
+        echo   "<div align='center'><a href='$blogpost->permalink'><img src='$thumbnail' /></a></div>";
+        echo   "<p><small>$subtitle</small></p>";
+        echo '</div>';
+    }
+
+
     /**
      * Show the given blogposts.
      *
@@ -20,7 +94,8 @@
         {
             if ($show_hidden_blogposts || (!$blogpost->draft && !$blogpost->deleted) )
             {
-                show_blogpost($blogpost);
+                echo "\n";
+                show_blogpost_summary($blogpost);
             }
         }
     }
