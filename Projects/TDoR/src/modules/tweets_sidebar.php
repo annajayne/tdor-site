d3f51a2a145ff06c13a69295d062f42841d13ee0
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2021-08-27 13:25:36 +0100

    Corrected the URL in the tweets sidebar

--- Projects/TDoR/src/modules/tweets_sidebar.php
+++ Projects/TDoR/src/modules/tweets_sidebar.php
@@ -6,7 +6,7 @@
 
       <!-- sidebar -->    
       <aside>
-        <a class="twitter-timeline" href="https://twitter.com/TDoRinfo?ref_src=twsrc%5Etfw">Tweets by TDoRinfo</a>
+        <a class="twitter-timeline" href="https://twitter.com/TDoRinfo">Tweets by TDoRinfo</a>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
       </aside><!-- #end sidebar -->
     
