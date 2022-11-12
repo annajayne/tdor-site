7abb3615a2ec1e230c6f8a28c2f25f9dccdd60f2
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2022-11-12 00:10:21 +0000

    Added TDoR 2022 downloads

--- Projects/TDoR/src/views/pages/downloads.php
+++ Projects/TDoR/src/views/pages/downloads.php
@@ -10,10 +10,48 @@
       echo '<p>&nbsp;</p>';
       echo '<div class="row">';
       echo   '<div class="grid_3">';
+      echo     '<img src="/downloads/tdor_2022_list_thumbnail.png" />';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2022 list of names [<a href="/reports/tdor2022?country=all&filter=&sortup=1&view=list" target="_blank">View</a>]<br>(tip: print to PDF and download)</p>';
+      echo   '</div>';
+      echo '</div>';
+
+      //echo '<div class="row">';
+      //echo   '<div class="grid_3">';
+      //echo     '<a href="/downloads/tdor_2022_victims.png" rel="lightbox" title="Just some of the trans people we have lost during the TDoR 2022 period"><img src="/downloads/tdor_2022_victims.png" /></a>';
+      //echo   '</div>';
+      //echo   '<div class="grid_9">';
+      //echo     '<p>TDoR 2022 victim photo collage [<a href="/downloads/tdor_2022_victims.png" download="tdor_2022_victims">Download</a>]</p>';
+      //echo   '</div>';
+      //echo '</div>';
+
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
+      echo     '<a href="/downloads/tdor_2022_map.png" rel="lightbox" title="Map showing the locations where trans people died during the TDoR 2022 period"><img src="/downloads/tdor_2022_map.png" /></a>';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2022 map of cases [<a href="/downloads/tdor_2022_map.png" download="tdor_2022_map">Download</a>]</p>';
+      echo   '</div>';
+      echo '</div>';
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
+      echo     '<a href="/downloads/tdor_2022_slides_thumbnail.png" rel="lightbox" title="A preview of the first page of the slides. Feel free to <a href=\'/reports/tdor2022?country=all&filter=&action=presentation&sortup=1\'>download</a> and edit them as needed"><img src="/downloads/tdor_2022_slides_thumbnail.png" /></a>';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2022 Powerpoint slides [<a href="/downloads/tdor_2022_slides.pptx" download="tdor_2022_slides">Download</a>]</p>';
+      echo   '</div>';
+      echo '</div>';
+
+      echo '<p>&nbsp;</p>';
+
+      echo '<p>&nbsp;</p>';
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
       echo     '<img src="/downloads/tdor_2021_list_thumbnail.png" />';
       echo   '</div>';
       echo   '<div class="grid_9">';
-      echo     '<p>TDoR 2021 list of names [<a href="/reports/tdor2021?country=all&filter=&sortup=1" target="_blank">View</a>]<br>(tip: print to PDF and download)</p>';
+      echo     '<p>TDoR 2021 list of names [<a href="/reports/tdor2021?country=all&filter=&sortup=1&view=list"" target="_blank">View</a>]<br>(tip: print to PDF and download)</p>';
       echo   '</div>';
       echo '</div>';
 
