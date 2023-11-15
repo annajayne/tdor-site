30aff572bc2283106ee2f617104c7d0cdb24b1f4
Anna-Jayne Metcalfe <anna-jayne@annasplace.me.uk>
2023-11-15 22:03:24 +0000

    Added TDoR 2023 downloads

--- Projects/TDoR/src/views/pages/downloads.php
+++ Projects/TDoR/src/views/pages/downloads.php
@@ -10,6 +10,44 @@
       echo '<p>&nbsp;</p>';
       echo '<div class="row">';
       echo   '<div class="grid_3">';
+      echo     '<img src="/downloads/tdor_2023_list_thumbnail.png" />';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2023 list of names [<a href="/reports/tdor2023?country=all&filter=&sortup=1&view=list" target="_blank">View</a>]<br>(tip: print to PDF and download)</p>';
+      echo   '</div>';
+      echo '</div>';
+
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
+      echo     '<a href="/downloads/tdor_2023_victims.png" rel="lightbox" title="Just some of the trans people we have lost during the TDoR 2023 period"><img src="/downloads/tdor_2023_victims.png" /></a>';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2023 victim photo collage [<a href="/downloads/tdor_2023_victims.png" download="tdor_2023_victims">Download</a>]</p>';
+      echo   '</div>';
+      echo '</div>';
+
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
+      echo     '<a href="/downloads/tdor_2023_map.png" rel="lightbox" title="Map showing the locations where trans people died during the TDoR 2023 period"><img src="/downloads/tdor_2023_map.png" /></a>';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2023 map of cases [<a href="/downloads/tdor_2023_map.png" download="tdor_2023_map">Download</a>]</p>';
+      echo   '</div>';
+      echo '</div>';
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
+      echo     '<a href="/downloads/tdor_2023_slides_thumbnail.png" rel="lightbox" title="A preview of the first page of the slides. Feel free to <a href=\'/reports/tdor2023?country=all&filter=&action=presentation&sortup=1\'>download</a> and edit them as needed"><img src="/downloads/tdor_2023_slides_thumbnail.png" /></a>';
+      echo   '</div>';
+      echo   '<div class="grid_9">';
+      echo     '<p>TDoR 2023 Powerpoint slides [<a href="/downloads/tdor_2023_slides.pptx" download="tdor_2023_slides">Download</a>]</p>';
+      echo   '</div>';
+      echo '</div>';
+
+      echo '<p>&nbsp;</p>';
+
+      echo '<p>&nbsp;</p>';
+      echo '<div class="row">';
+      echo   '<div class="grid_3">';
       echo     '<img src="/downloads/tdor_2022_list_thumbnail.png" />';
       echo   '</div>';
       echo   '<div class="grid_9">';
